ff230ba76e5474189b284d78491256b2eceff51c
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-06-12 18:46:26 +0000

    add code for the strech assignment

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,4 +1,4 @@
-let number = 100;
+/*let number = 100;
 let output;
 for (let i = 0; i < 100; i++) {
   if (number % 3 === 0 && number % 4 === 0) {
@@ -12,4 +12,23 @@ for (let i = 0; i < 100; i++) {
   }
   console.log(output);
   number += 1;
-}  
+} 
+*/
+
+const loopyLighthouse = function (range, multiples, words) {
+  let output;
+  for (let i = range[0]; i <= range[1]; i++) {
+    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
+      output = words[0] + words[1]
+    } else if (i % multiples[0] === 0) {
+      output = words[0];
+    } else if (i % multiples[1] === 0) {
+      output = words[1];
+    } else {
+      output = i;
+    }
+    console.log(output);
+  }
+}
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
